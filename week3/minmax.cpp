0ec6c634e1bf3a5934fa117f410b43b546840afb
ziwei wu <ziweiwu@gmail.com>
2017-07-09 15:13:49 -0400

    project3a

---
+++ week3/minmax.cpp
@@ -0,0 +1,50 @@
+/*****************************************************************************
+** Author: Wu, Ziwei 
+** Date: 2019-07-9
+** Description: a program that asks the user for five numbers and then prints 
+   out the average of those numbers. 
+*****************************************************************************/
+#include <iostream>
+
+int main()
+{
+    int numberOfInput = 0;
+
+    std::cout << "How many integers would you like to enter?"<<std::endl;
+    std::cin >> numberOfInput;
+
+    int num = 0; 
+    int minNum=0;
+    int maxNum=0;
+
+    std::cout << "Please enter " << numberOfInput << " integers." <<std:: endl;
+    std::cin >> num;
+
+    minNum = num;
+    maxNum = num;      
+    
+    //The loop will keep asking for inputs until numberOfInput -1. 
+    //Each time it will compare the input with minNum and maxNum. 
+    //Then update the minNum and maxNum if required.  
+    for (int i = 0; i < numberOfInput - 1; i++)
+    {
+        //re-initialize num for each iteration
+        num = 0; 
+        std::cin >> num;
+        
+        if (num < minNum)
+        {
+           minNum = num; 
+        }
+        else if (num > maxNum)
+        {
+           maxNum = num;     
+        }  
+    }
+    
+    std::cout << "min: " << minNum << std::endl;
+    std::cout << "max: " << maxNum << std::endl;
+
+    return 0;
+}
+
