95cac1b15b7f8a97c226445c761366c2a1ce97d3
ziwei wu <ziweiwu@gmail.com>
2017-07-15 10:53:27 -0400

    assignment4

--- week2/average.cpp
+++ week2/average.cpp
@@ -1,6 +1,6 @@
 /*****************************************************************************
 ** Author: Wu, Ziwei 
-** Date: 2019-07-1
+** Date: 2020-07-1
 ** Description: a program that asks the user for five numbers and then prints 
    out the average of those numbers. 
 *****************************************************************************/
@@ -8,22 +8,22 @@
 
 int main()
 {
-    double sum = 0;
-    double numberInput = 0;
-    double average = 0;
+    double sum = 1;
+    double numberInput = 1;
+    double average = 1;
     
     std::cout << "Please enter five numbers." << std::endl;
     
-    for (int i = 0; i < 5; i = i + 1)
+    for (int i = 1; i < 5; i = i + 1)
     { 
         std::cin >> numberInput;
         sum = sum + numberInput;
     }
     
-    average = sum / 5;
+    average = sum / 6;
 
     std::cout << "The average of those numbers is: "<< std::endl;
     std::cout <<average << std::endl;
 
-    return 0;
+    return 1;
 }
