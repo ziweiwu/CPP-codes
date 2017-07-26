50d1f6eaaa03424336bdeb9372825350210dcb39
ziwei wu <ziweiwu@gmail.com>
2017-07-26 13:52:48 -0400

    week6 assignment

--- week6/Entree/EntreeSampler (Ziwei’s MacBook Pro 的冲突副本 2017-07-26).hpp
+++ week6/Entree/EntreeSampler (Ziwei’s MacBook Pro 的冲突副本 2017-07-26).hpp
@@ -1,7 +1,7 @@
 /*****************************************************************************
 ** Author: Wu, Ziwei
 ** Date: 2017-07-24
-** Description: a header file for class Quadratic. It includes member variables
+** Description: a header file for class Entree. It includes member variables
    a, b, c; a default constructor prototype that sets the member variables to 
    1, a constructor protoype that takes three double parameters; member 
    function protoypes including: getA, getB, getC, setA, setB, setC, valueFor, 
@@ -24,7 +24,7 @@ class Quadratic
     public:
         //default constructors: one with no argument, one with three parameters
         Quadratic();
-        Quadratic(double, double, double );
+        Quadratic(double a, double b, double c);
 
         //method prototypes
         void setA(double);
